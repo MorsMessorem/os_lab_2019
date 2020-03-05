8de03ed1b6465d8a40d208eb56a1556f20a5bffc
morsmessorem <smyrfik1999@gmail.com>
2020-03-05 16:55:17 +0000

    lab2.1

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,19 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    int a=strlen(str);
+    char *s = malloc(sizeof(char) * (a + 1));
+    int i=0;
+    while (i<a)
+    {
+        s[i]=str[i];
+        i++;
+    }
+    i=0;
+    while (i<a)
+    {
+        str[i]=s[a-i-1];
+        i++;
+    }
 }
 
